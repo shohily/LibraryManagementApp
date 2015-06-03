ab510546514de66348616fb1561280b026444319
shohily <shohely.haque@gmail.com>
2015-06-03 09:26:56 -0700

    Libray management

---
+++ LibraryManagementApp/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace LibraryManagementApp
+{
+    partial class LibraryManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.bookNameTextBox = new System.Windows.Forms.TextBox();
+            this.authorNameTextBox = new System.Windows.Forms.TextBox();
+            this.bookIdTextBox = new System.Windows.Forms.TextBox();
+            this.editionTextBox = new System.Windows.Forms.TextBox();
+            this.genreTextBox = new System.Windows.Forms.TextBox();
+            this.save = new System.Windows.Forms.Button();
+            this.show = new System.Windows.Forms.Button();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Book Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Book Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Author Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 202);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Genre";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Edition";
+            // 
+            // bookNameTextBox
+            // 
+            this.bookNameTextBox.Location = new System.Drawing.Point(117, 75);
+            this.bookNameTextBox.Name = "bookNameTextBox";
+            this.bookNameTextBox.Size = new System.Drawing.Size(116, 21);
+            this.bookNameTextBox.TabIndex = 5;
+            // 
+            // authorNameTextBox
+            // 
+            this.authorNameTextBox.Location = new System.Drawing.Point(117, 125);
+            this.authorNameTextBox.Name = "authorNameTextBox";
+            this.authorNameTextBox.Size = new System.Drawing.Size(116, 21);
+            this.authorNameTextBox.TabIndex = 6;
+            // 
+            // bookIdTextBox
+            // 
+            this.bookIdTextBox.Location = new System.Drawing.Point(117, 27);
+            this.bookIdTextBox.Name = "bookIdTextBox";
+            this.bookIdTextBox.Size = new System.Drawing.Size(116, 21);
+            this.bookIdTextBox.TabIndex = 7;
+            // 
+            // editionTextBox
+            // 
+            this.editionTextBox.Location = new System.Drawing.Point(117, 166);
+            this.editionTextBox.Name = "editionTextBox";
+            this.editionTextBox.Size = new System.Drawing.Size(116, 21);
+            this.editionTextBox.TabIndex = 8;
+            // 
+            // genreTextBox
+            // 
+            this.genreTextBox.Location = new System.Drawing.Point(117, 202);
+            this.genreTextBox.Name = "genreTextBox";
+            this.genreTextBox.Size = new System.Drawing.Size(116, 21);
+            this.genreTextBox.TabIndex = 9;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(117, 250);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(87, 27);
+            this.save.TabIndex = 10;
+            this.save.Text = "Save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(716, 250);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(87, 27);
+            this.show.TabIndex = 11;
+            this.show.Text = "Show";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView.Location = new System.Drawing.Point(268, 27);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(535, 197);
+            this.dataGridView.TabIndex = 12;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "BookId";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "BookName";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "AuthorName";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Edition";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Genre";
+            this.Column5.Name = "Column5";
+            // 
+            // LibraryManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(820, 301);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.genreTextBox);
+            this.Controls.Add(this.editionTextBox);
+            this.Controls.Add(this.bookIdTextBox);
+            this.Controls.Add(this.authorNameTextBox);
+            this.Controls.Add(this.bookNameTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "LibraryManagement";
+            this.Text = "Library Management";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox bookNameTextBox;
+        private System.Windows.Forms.TextBox authorNameTextBox;
+        private System.Windows.Forms.TextBox bookIdTextBox;
+        private System.Windows.Forms.TextBox editionTextBox;
+        private System.Windows.Forms.TextBox genreTextBox;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ LibraryManagementApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagementApp
+{
+    public partial class LibraryManagement : Form
+    {
+        public LibraryManagement()
+        {
+            InitializeComponent();
+        }
+
+
+         string connectionstring = @"server =USER;database=DB_LibraryBookManagement;integrated security=true";
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            string bookId = bookIdTextBox.Text;
+            string bookName = bookNameTextBox.Text;
+            string authorName = authorNameTextBox.Text;
+            string edition = editionTextBox.Text;
+            string genre = genreTextBox.Text;
+
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+
+            string query = string.Format(@"insert into LibraryManagement_Table(BookId,BookName,AuthorName,Edition,Genre)
+                  values('{0}','{1}','{2}','{3}','{4}')", bookId, bookName, authorName, edition, genre);
+            SqlCommand aCommand = new SqlCommand(query, aConnection);
+
+            MessageBox.Show("Data has been inserted");
+            bookIdTextBox.Text = "";
+            bookNameTextBox.Text = "";
+            authorNameTextBox.Text = "";
+            editionTextBox.Text = "";
+            genreTextBox.Text = "";
+
+            aCommand.ExecuteNonQuery();
+
+            aConnection.Close();
+
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            dataGridView.Refresh();
+            SqlConnection aConnection = new SqlConnection(connectionstring);
+            aConnection.Open();
+
+            string tableSql = "SELECT * FROM LibraryManagement_Table";
+            SqlCommand selectAllTableSqlCommand = new SqlCommand(tableSql, aConnection);
+
+            SqlDataReader tableReader = selectAllTableSqlCommand.ExecuteReader();
+
+            if (tableReader != null)
+                while (tableReader.Read())
+                {
+                    dataGridView.Rows.Add(tableReader[0].ToString(), tableReader[1].ToString(),
+                                                 tableReader[2].ToString(), tableReader[3].ToString(),
+                                                 tableReader[4].ToString());
+                }
+            tableReader.Close();
+            aConnection.Close();
+ 
+        }
+    }
+}
